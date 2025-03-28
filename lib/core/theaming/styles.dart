7c0e2d526dafe953f2d9a459429c4be04f88b3c5
AbdallahElbetar <abdallahelbetar6@gmail.com>
2025-03-28 03:40:19 +0200

    add new Text Style

--- lib/core/theaming/styles.dart
+++ lib/core/theaming/styles.dart
@@ -12,11 +12,16 @@ abstract class Styles {
       fontFamily: GoogleFonts.readexPro().fontFamily);
   static final textStyleGrey16 = TextStyle(
       fontSize: 16.sp, fontWeight: FontWeight.w400, color: AppColors.myGrey);
-  static final textStyleBlack16 = TextStyle(
+  static final textStyleBlack16ReadexProFont = TextStyle(
       fontSize: 16.sp,
       fontWeight: FontWeight.w500,
       color: AppColors.myBlack,
       fontFamily: GoogleFonts.readexPro().fontFamily);
+  static final textStyleBlack16DmSansFont = TextStyle(
+      fontSize: 16.sp,
+      fontWeight: FontWeight.w500,
+      color: AppColors.myBlack,
+      fontFamily: GoogleFonts.dmSans().fontFamily);
   static final textStyleWhite14 = TextStyle(
       fontSize: 14.sp, fontWeight: FontWeight.w500, color: AppColors.myWhite);
   static final textStyleWhite16 = TextStyle(

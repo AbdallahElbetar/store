2a4f7500941c24466aec98fc9e4efffdf95f0290
AbdallahElbetar <abdallahelbetar6@gmail.com>
2025-03-29 06:58:37 +0200

    add homeView Router and check User Logged in

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,28 +1,54 @@
 import 'package:flutter/widgets.dart';
 import 'package:go_router/go_router.dart';
-import 'package:store/features/home/presentation/view/views/home_view.dart';
-
+import '../di/setup_services_locator.dart';
+import '../utils/storage_helper.dart';
+import '../../features/home/presentation/view/views/home_view.dart';
 import '../../features/auth/presentation/view/views/login_view.dart';
 import '../../features/auth/presentation/view/views/register_view.dart';
 
 class AppRouter {
-  static final String kRegisterView = '/registerView';
-  static final String kHomeView = '/homeView';
-  static final GoRouter router = GoRouter(routes: [
-    // GoRoute(
-    //     path: '/',
-    //     builder: (BuildContext context, GoRouterState state) {
-    //       return const LoginView();
-    //     }),
-    GoRoute(
+  static const String kRegisterView = '/registerView';
+  static const String kHomeView = '/homeView';
+  static const String kLoginView = '/loginView';
+
+  static Future<String> getInitialLocation() async {
+    String? token = await getIt<StorageHelper>().getToken();
+    if (token != null) {
+      return kHomeView;
+    } else {
+      return kLoginView;
+    }
+  }
+
+  static final GoRouter router = GoRouter(
+    initialLocation: kLoginView,
+    routes: [
+      GoRoute(
+        path: kHomeView,
+        builder: (BuildContext context, GoRouterState state) {
+          return const HomeView();
+        },
+      ),
+      GoRoute(
+        path: kLoginView,
+        builder: (BuildContext context, GoRouterState state) {
+          return const LoginView();
+        },
+      ),
+      GoRoute(
         path: kRegisterView,
         builder: (BuildContext context, GoRouterState state) {
           return const RegsiterView();
-        }),
-    GoRoute(
-        path: '/',
-        builder: (BuildContext context, GoRouterState state) {
-          return const HomeView();
-        }),
-  ]);
+        },
+      ),
+    ],
+    redirect: (BuildContext context, GoRouterState state) async {
+      String initialRoute = await getInitialLocation();
+
+      if (state.uri.toString() != initialRoute) {
+        return initialRoute;
+      }
+      return null;
+    },
+  );
 }

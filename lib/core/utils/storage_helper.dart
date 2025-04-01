86687274326d9fe662da1b2484d1d6ee415017a7
AbdallahElbetar <abdallahelbetar6@gmail.com>
2025-04-01 05:31:08 +0200

    refactor Storage Helper and add IOS Option For encrypted Data

--- lib/core/utils/storage_helper.dart
+++ lib/core/utils/storage_helper.dart
@@ -1,16 +1,20 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class StorageHelper {
-  final storage = const FlutterSecureStorage(
-    aOptions: AndroidOptions(encryptedSharedPreferences: true),
-  );
+  final storage = FlutterSecureStorage(
+      aOptions: const AndroidOptions(
+        encryptedSharedPreferences: true,
+      ),
+      iOptions: IOSOptions(
+        accessibility: KeychainAccessibility.first_unlock,
+      ));
 
-  Future<void> saveToken(String token) async {
+  Future<void> saveToken({required String token}) async {
     await storage.write(key: "token", value: token);
   }
 
-  Future<String> getToken() async {
-    return await storage.read(key: "token") ?? "";
+  Future<String?> getToken() async {
+    return await storage.read(key: "token");
   }
 
   Future<void> deleteToken() async {
